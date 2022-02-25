35a7728b1a6c41d69fa18cf571c21fdaab2096d1
94sylim <94sylim@gmail.com>
2022-02-25 14:10:17 +0900

    회원 객체

---
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,28 @@
+package hello.hellospring.domain;
+
+import javax.persistence.*;
+
+@Entity // JPA 관리
+public class Member {
+
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 알아서 생성
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
